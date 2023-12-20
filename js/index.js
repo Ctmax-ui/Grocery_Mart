767821a48c466d0122125e02c339394f2b7491ee
Debjeet <68694444+Ctmax-ui@users.noreply.github.com>
2023-12-20 21:34:49 +0530

    added new modal boxes

--- js/index.js
+++ js/index.js
@@ -1,67 +1,87 @@
-//For The User Login And Password Modal
+//For the items cart...................
+const itemsBtnBox = document.querySelector(".items-cart-btn");
+const itemBox = document.querySelector(".items-cart-container");
+itemsBtnBox.addEventListener("click", onclickItems => {
+    itemBox.style = "display: block; !importent";
+    const itemsCloseIcn = document.querySelector(".items-cart-close-icn")
+    itemsCloseIcn.addEventListener("click", (f) => {
+        itemBox.style = "";
+    });
+});
+
+
+
+// const allCartBtn = document.querySelectorAll(".grocery-card-button");
+// console.log(allCartBtn);
+
+// allCartBtn.forEach( e =>{
+    
+//     e.addEventListener("click", s=>{
+//         console.log(s);
+//     });
+
+// });
+
+
+
+
+//For the search modal...............
+const searchModal = document.querySelector(".search-modal-btn");
+const userSearch = document.querySelector(".search-modal-container");
+searchModal.addEventListener("click", onclickSearch => {
+    // console.log(onclickSearch);
+    console.log(userSearch);
+    userSearch.style = "display: block; !importent";
+    userSearch.addEventListener("click", (f) => {
+        userSearch.style = "";
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+//For The User Login And Password Modal..............
 const userLoginModal = document.querySelector(".user-login-modal");
 
 userLoginModal.addEventListener("click", userOnclick => {
-const userModal= document.querySelector(".user-modal");
-console.log(userModal);
-userModal.style= "display: block; !importent";
-    userModal.addEventListener("click", (f)=>{
-        userModal.style= "";
+    const userModal = document.querySelector(".user-modal");
+    console.log(userModal);
+    userModal.classList.add("user-modal-show");
+    userModal.addEventListener("click", (f) => {
+        userModal.classList.remove("user-modal-show");
     });
 });
 
 
-
+//For the login and create Account tap/pill
 const btnGroup = document.querySelectorAll(".zx-btn");
-        const tabGroup = document.querySelectorAll(".zx-tabs");
+const tabGroup = document.querySelectorAll(".zx-tabs");
 
-        tabGroup.forEach((eachTabs) => {
+tabGroup.forEach((eachTabs) => {
 
-            btnGroup.forEach((eachBtn) => {
+    btnGroup.forEach((eachBtn) => {
 
-                eachBtn.addEventListener("click", () => {
+        eachBtn.addEventListener("click", () => {
 
-                    eachTabs.classList.remove("zx-tabs-show");
-                    //console.log(eachBtn);
+            eachTabs.classList.remove("zx-tabs-show");
+            //console.log(eachBtn);
 
-                    if (+eachBtn.dataset.fortab === +eachTabs.dataset.tab) {
-                        btnGroup.forEach(btn => { btn.classList.remove("btn-show") });
+            if (+eachBtn.dataset.fortab === +eachTabs.dataset.tab) {
+                btnGroup.forEach(btn => { btn.classList.remove("btn-show") });
 
-                        eachTabs.classList.add("zx-tabs-show");
-                        eachBtn.classList.add("btn-show");
-                    };
-                });
-            });
+                eachTabs.classList.add("zx-tabs-show");
+                eachBtn.classList.add("btn-show");
+            };
         });
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const allCartBtn = document.querySelectorAll(".grocery-card-button");
-console.log(allCartBtn);
-
-allCartBtn.forEach( e =>{
-    
-    e.addEventListener("click", s=>{
-        console.log(s);
     });
-
 });
-*/
-
-
-
-
 
 
 
@@ -81,7 +101,7 @@ const headerSection = document.querySelector(".section-first-header");
 const mainNav = document.querySelector(".site-navigation-header");
 const mainNavText = document.querySelectorAll(".nav-navigation-text");
 
-const observeSect = new IntersectionObserver((entities)=>{
+const observeSect = new IntersectionObserver((entities) => {
     // console.log(entities[0].isIntersecting);
 
     mainNav.classList.toggle("fixed-top", !entities[0].isIntersecting);
@@ -96,7 +116,7 @@ const observeSect = new IntersectionObserver((entities)=>{
         element.classList.toggle("text-black", !entities[0].isIntersecting);
     });
 
-}, {/*rootMargin: "1px"*/});     
+}, {/*rootMargin: "1px"*/ });
 
 
 observeSect.observe(headerSection);
@@ -106,21 +126,21 @@ observeSect.observe(headerSection);
 
 //For the owl carousel
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    autoplay:true,
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
     autoplayTimeout: 2000,
-    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
-    responsive:{
-        0:{
-            items:3
+    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    responsive: {
+        0: {
+            items: 3
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:4
+        1000: {
+            items: 4
         }
     }
 });
