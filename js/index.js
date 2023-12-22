3ae4f6b364635b42ef30baed3823636e1762ad41
Debjeet <68694444+Ctmax-ui@users.noreply.github.com>
2023-12-22 21:44:31 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 //For the items cart...................
 const itemsBtnBox = document.querySelector(".items-cart-btn");
 const itemBox = document.querySelector(".items-cart-container");
@@ -10,17 +11,120 @@ itemsBtnBox.addEventListener("click", onclickItems => {
 });
 
 
+const mainItemBox = document.querySelector(".items-cart-box table ");
 
-// const allCartBtn = document.querySelectorAll(".grocery-card-button");
-// console.log(allCartBtn);
 
-// allCartBtn.forEach( e =>{
-    
-//     e.addEventListener("click", s=>{
-//         console.log(s);
-//     });
+// import { allProducts } from './itemsDetails.js';
+// console.log(allProducts);
 
-// });
+const allProducts = [
+    {
+        name: "Ladies Finger 500g",
+        price: 350,
+    },
+    {
+        name: "Brinjal long 500g",
+        price: 300,
+    },
+    {
+        name: "Palak 250g",
+        price: 245,
+    },
+
+    // Add other products and their prices here if needed
+];
+
+
+const allCartBtn = document.querySelectorAll(".grocery-card-button");
+allCartBtn.forEach(eachCartBtn => {
+
+    eachCartBtn.addEventListener("click", onClickEachBtn => {
+        // console.log(typeof eachCartBtn.dataset.items, eachCartBtn.dataset.items);
+
+        let dataSetValue = eachCartBtn.dataset.items;
+        console.log(dataSetValue);
+
+        /*<------------------->*/
+        // 1 Ladies Finger value.
+        const ladiesFingerProduct = allProducts.find(product => product.name === "Ladies Finger 500g");
+        const ladiesFingerHTML = `<tr><td>${ladiesFingerProduct.name}</td><td>x1</td><td>$${ladiesFingerProduct.price}</td></tr>`;
+        
+        // add more value.
+        const brinjalLongProduct = allProducts.find(product => product.name === "Brinjal long 500g");
+        const brinjalLongHTML = `<tr><td>${brinjalLongProduct.name}</td><td>x1</td><td>$${brinjalLongProduct.price}</td></tr>`;
+
+
+        /*<------------------->*/
+
+
+        if (onClickEachBtn.dataset = 0) {
+            console.log("Nothing = Null");
+        }
+
+        /* <----------All Product If else---------->*/
+
+        else if (dataSetValue === "ladiesFinger") {
+
+            // const existingProductRow = mainItemBox.innerText;
+            const existingladiesFingerRow = Array.from(mainItemBox.children).find(row => row.innerHTML.includes(ladiesFingerProduct.name));
+            if (existingladiesFingerRow) {
+                const countladiesFinger = existingladiesFingerRow.querySelector('td:nth-child(2)');
+                const currentladiesFingerCount = parseInt(countladiesFinger.innerText.slice(1));
+                countladiesFinger.innerText = `x${currentladiesFingerCount + 1}`;
+                // console.log("Total Ladies Finger:", currentladiesFingerCount + 1); 
+            }
+            else {
+                console.log("ladiesFinger added");
+                mainItemBox.innerHTML += ladiesFingerHTML;
+                
+            };
+        }
+
+//         2nd
+
+        else if (dataSetValue === "brinjalLong") {
+            
+            const existingBrinjalLongRow = Array.from(mainItemBox.children).find(row => row.innerHTML.includes(brinjalLongProduct.name));
+
+            if (existingBrinjalLongRow) {
+                const countBrinjalLong = existingBrinjalLongRow.querySelector('td:nth-child(2)');
+                const currentBrinjalLongCount = parseInt(countBrinjalLong.innerText.slice(1));
+                countBrinjalLong.innerText = `x${currentBrinjalLongCount + 1}`;
+                // console.log("Total Ladies Finger:", currentladiesFingerCount + 1); 
+            }
+            else {
+                console.log("brinjalLong added");
+                mainItemBox.innerHTML += brinjalLongHTML;
+                
+            };
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        else {
+            console.log("0");
+        }
+        
+        
+    });
+});
+
+
+
+
+// mainItemBox.innerHTML +=("<tr><td>Toma Cookies</td><td>x1</td><td>300$</td></tr>");
+
+
+
+
+
 
 
 
@@ -30,7 +134,7 @@ const searchModal = document.querySelector(".search-modal-btn");
 const userSearch = document.querySelector(".search-modal-container");
 searchModal.addEventListener("click", onclickSearch => {
     // console.log(onclickSearch);
-    console.log(userSearch);
+    // console.log(userSearch);
     userSearch.style = "display: block; !importent";
     userSearch.addEventListener("click", (f) => {
         userSearch.style = "";
@@ -52,7 +156,7 @@ const userLoginModal = document.querySelector(".user-login-modal");
 
 userLoginModal.addEventListener("click", userOnclick => {
     const userModal = document.querySelector(".user-modal");
-    console.log(userModal);
+    // console.log(userModal);
     userModal.classList.add("user-modal-show");
     userModal.addEventListener("click", (f) => {
         userModal.classList.remove("user-modal-show");
---
+++ js/itemsDetails.js
@@ -0,0 +1,16 @@
+export const allProducts = [
+        {
+            name: "Ladies Finger",
+            price: 350,
+        },
+        {
+            name: "Ladies Finger",
+            price: 350,
+        },
+        {
+            name: "Ladies Finger",
+            price: 350,
+        },
+    
+        // Add other products and their prices here if needed
+    ];
